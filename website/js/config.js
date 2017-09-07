34b800a240069200c0f0012512b56b788419672d
Simon Crossley <scrossley@mylifedigital.co.uk>
2017-09-07 16:31:10 +0100

    Add basic website

---
+++ website/js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+	auth0: {
+		domain: 'simon-mld-us-test.auth0.com',
+		clientId: 'yNe5uEpHFyaU2SCDN6gHmzPeqSZwdfcl'
+	},
+  apiBaseUrl: 'https://qcwed3uf83.execute-api.eu-west-1.amazonaws.com/dev'
+};
